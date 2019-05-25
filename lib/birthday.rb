cf2f2c18c19c80003fed06b45286debd719ebea9
Tom White <eugene.white@gmail.com>
2019-05-25 15:07:51 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -5,10 +5,10 @@
 # }
 
 #def happy_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
+ # birthday_kids.each do |kids_name, age|
+ #   puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
+ # end
+#end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
